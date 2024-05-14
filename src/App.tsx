27fcd67acc41ae8ab7eafc3ca908c0d1e1db0e91
Th-Sagar <sagarthapa98122@gmail.com>
2024-05-14 08:13:13 +0545

    feat:shadecn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <>
       <h1>Welcome to elib dashboard</h1>
+      <Button >
+        submit
+      </Button>
     </>
   );
 }
