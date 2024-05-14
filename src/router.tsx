ace46e9154c529130ffdd4ce9f86ef2d04d62d5d
Th-Sagar <sagarthapa98122@gmail.com>
2024-05-14 17:02:10 +0545

    feat:register

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,8 @@
 import {createBrowserRouter} from 'react-router-dom'
-import LoginPage from '@/pages/Login'
+
 import HomePage from '@/pages/HomePage'
+import LoginPage from '@/pages/LoginPage'
+import RegisterPage from '@/pages/RegisterPage'
 
  const router =  createBrowserRouter([
 
@@ -11,7 +13,10 @@ import HomePage from '@/pages/HomePage'
     {
         path:'/login',
         element:<LoginPage/>
-    }
+    },{
+        path:'/register',
+        element:<RegisterPage/>
+    },
 ])
 
 
