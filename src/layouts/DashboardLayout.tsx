05f61f98724ff57e2e8dcfc0c69f62c490843eff
Th-Sagar <sagarthapa98122@gmail.com>
2024-05-27 23:24:42 +0545

    feat: secure route

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -17,6 +17,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import useTokenStore from "@/store";
 
 import {
   Bell,
@@ -30,14 +31,22 @@ import {
   ShoppingCart,
   Users,
 } from "lucide-react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, Navigate } from "react-router-dom";
 const DashboardLayout = () => {
+  const token = useTokenStore((state) => state.token);
+
+  if (token === "") {
+    return <Navigate to="/auth/login" replace />;
+  }
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-            <Link to={"/dashboard/home"} className="flex items-center gap-2 font-semibold">
+            <Link
+              to={"/dashboard/home"}
+              className="flex items-center gap-2 font-semibold"
+            >
               <Package2 className="h-6 w-6" />
               <span className="">Coder's Book</span>
             </Link>
@@ -192,14 +201,11 @@ const DashboardLayout = () => {
           </DropdownMenu>
         </header>
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-        <Outlet/>
-
-
-          
+          <Outlet />
         </main>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardLayout
+export default DashboardLayout;
